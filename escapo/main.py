116cc3685549b62a473658aad229db646260b9c0
alybali <alybalistreri@gmail.com>
2024-06-30 21:30:31 -0500

    escapo v2

--- escapo/main.py
+++ escapo/main.py
@@ -1,9 +1,21 @@
 import subprocess
+import sys
+
+def check_wmctrl():
+    try:
+        subprocess.run(['wmctrl', '-m'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        print("wmctrl is not installed. Please install it using your package manager.")
+        sys.exit(1)
+    except FileNotFoundError:
+        print("wmctrl is not found. Please install it using your package manager.")
+        sys.exit(1)
 
 def minimize_all_windows():
+    check_wmctrl()
     try:
-        # Use xdotool to minimize all open windows
-        subprocess.run(['xdotool', 'search', '--onlyvisible', '--class', '', 'windowminimize'], check=True)
+        # Use wmctrl to minimize all open windows
+        subprocess.run(['wmctrl', '-k', 'on'], check=True)
         print("All windows minimized successfully.")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sudo_escapo',
+    name='escapo',
     version='0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'escapo=sudo_escapo.main:minimize_all_windows',
-            'sudo_escapo=sudo_escapo.main:minimize_all_windows',
+            'escapo=escapo.main:minimize_all_windows',
+            'sudo_escapo=escapo.main:minimize_all_windows',
         ],
     },
     install_requires=[
@@ -15,7 +15,8 @@ setup(
     ],
     description='A script to minimize all open windows',
     author='Swiss',
-    url='https://github.com/alybali/sudo-escapo'
+    url='https://github.com/alybali/sudo-escapo',
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
